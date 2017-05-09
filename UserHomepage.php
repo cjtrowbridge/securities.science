ce0d7ba2ba246bdca8cc8468584095d9134ede6c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-09 10:44:36 -0700

    Update UserHomepage.php

--- UserHomepage.php
+++ UserHomepage.php
@@ -6,7 +6,8 @@ function UserHomepageBodyCallback(){
 <div class="container">
   <div class="row no-gutters">
     <div class="col-md-12">
-      <p>Welcome to securities.science. Check us out on <a href="https://github.com/cjtrowbridge/securities.science" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to imrpove this tool!</p>
+      <h4>Welcome to securities.science</h4>
+      <p>Check us out on <a href="https://github.com/cjtrowbridge/securities.science" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to imrpove this tool!</p>
     
       <!-- Nav tabs -->
       <ul class="nav nav-tabs" role="tablist">
