2314e54b373a71eb3f7b0a3a8c78fc538cb93ac0
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-09 11:22:18 -0700

    Create PublicExplore.php

---
+++ PublicExplore.php
@@ -0,0 +1,21 @@
+<?php
+
+function PublicExploreBodyCallback(){
+  ?>
+
+<div class="container">
+  <div class="row no-gutters">
+    <div class="col-md-12">
+      <h4>Explore securities.science</h4>
+      <p>Check us out on <a href="https://github.com/cjtrowbridge/securities.science" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to improve this tool!</p>
+    
+      <p>List popular users and suggest new people to follow.</p>
+      <p>Show their queries and feedback. suggest login in order to provide feedback.</p>
+      
+      
+    </div>
+  </div>
+</div>
+
+  <?php
+}
