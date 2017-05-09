fbd76f1e313d88da5bd21b23467692f127cf3b5f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-09 10:35:34 -0700

    Update main.php
    Added user homepage hook

--- main.php
+++ main.php
@@ -6,3 +6,11 @@ function PublicHomepage(){
   include('PublicHomepage.php');
   TemplateBootstrap4('home','PublicHomepageBodyCallback();');
 }
+
+
+Hook('User Is Logged In','UserHomepage();');
+
+function UserHomepage(){
+  include('UserHomepage.php');
+  TemplateBootstrap4('home','UserHomepageBodyCallback();');
+}
