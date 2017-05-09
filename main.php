eb29b6c658bbbd2cff1c49503b2affc869fa5c0b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-09 10:50:36 -0700

    Update main.php
    Moved to switch statements to handle routing

--- main.php
+++ main.php
@@ -1,16 +1,33 @@
 <?php
 
-Hook('User Is Not Logged In','PublicHomepage();');
+Hook('User Is Not Logged In','PublicPage();');
 
-function PublicHomepage(){
-  include('PublicHomepage.php');
-  TemplateBootstrap4('home','PublicHomepageBodyCallback();');
+function PublicPage(){
+  switch(path(0)){
+    case 'explore':
+      include('PublicExplore.php');
+      TemplateBootstrap4('explore','PublicExploreBodyCallback();');
+      break;
+    default:
+      include('PublicHomepage.php');
+      TemplateBootstrap4('home','PublicHomepageBodyCallback();');
+      break;
+  }
 }
 
 
-Hook('User Is Logged In','UserHomepage();');
+Hook('User Is Logged In','UserPage();');
 
-function UserHomepage(){
-  include('UserHomepage.php');
-  TemplateBootstrap4('home','UserHomepageBodyCallback();');
+function UserPage(){
+  switch(path(0)){
+    case 'explore':
+      include('UserExplore.php');
+      TemplateBootstrap4('explore','UserExploreBodyCallback();');
+      break;
+    default:
+      include('UserHomepage.php');
+      TemplateBootstrap4('home','UserHomepageBodyCallback();');
+      break;
+  }
+  
 }
