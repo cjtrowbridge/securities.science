a89dda75fc146743f0ac642992c4a4cc66a71700
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-09 10:46:38 -0700

    Update UserHomepage.php
    I decided I didn't like this layout and moved to nav pills instead.

--- UserHomepage.php
+++ UserHomepage.php
@@ -9,28 +9,8 @@ function UserHomepageBodyCallback(){
       <h4>Welcome to securities.science</h4>
       <p>Check us out on <a href="https://github.com/cjtrowbridge/securities.science" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to imrpove this tool!</p>
     
-      <!-- Nav tabs -->
-      <ul class="nav nav-tabs" role="tablist">
-        <li class="nav-item">
-          <a class="nav-link active" data-toggle="tab" href="#MyData" role="tab">My Data</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-toggle="tab" href="#Explore" role="tab">Explore</a>
-        </li>
-      </ul>
-
-      <!-- Tab panes -->
-      <div class="tab-content">
-        <div class="tab-pane active" id="MyData" role="tabpanel">
-          My Data
-        </div>
-        <div class="tab-pane" id="Explore" role="tabpanel">
-          Explore
-          
-          <p>List people you follow here</p>
-          <p>List new and suggested users here</p>
-        </div>
-      </div>
+      <p>List my queries and data, offer to create a new query.</p>
+      
       
     </div>
   </div>
