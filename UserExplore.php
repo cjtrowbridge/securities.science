089c1dad9833d26e443728c4e6e6f69177ebf8d8
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-09 10:47:52 -0700

    Create UserExplore.php

---
+++ UserExplore.php
@@ -0,0 +1,21 @@
+<?php
+
+function UserExploreBodyCallback(){
+  ?>
+
+<div class="container">
+  <div class="row no-gutters">
+    <div class="col-md-12">
+      <h4>Explore securities.science</h4>
+      <p>Check us out on <a href="https://github.com/cjtrowbridge/securities.science" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to imrpove this tool!</p>
+    
+      <p>List people I folow and suggest new people to follow.</p>
+      <p>Show their queries and feedback. Allow users to provide feedback.</p>
+      
+      
+    </div>
+  </div>
+</div>
+
+  <?php
+}
