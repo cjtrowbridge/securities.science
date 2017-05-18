b8125a1454643985631fe142f0f5ca9d2d0aabb0
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-18 10:39:59 -0700

    Update ShowSecuritiesScienceScraperStats.php

--- ShowSecuritiesScienceScraperStats.php
+++ ShowSecuritiesScienceScraperStats.php
@@ -4,6 +4,6 @@ function ShowSecuritiesScienceScraperStats(){
   ?>
   <h2>Scraper Stats</h2>
   <?php
-  $Stats=Query('SELECT * FROM Statistics','stockhistory');
-  echo ArrTabler($Stats);
+  $Code="echo ArrTabler(Query('SELECT * FROM Statistics','stockhistory'));";
+  ace($Code);
 }
