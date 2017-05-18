82d6a53bcb3fb9c1b2d22c9a1090ab9b8b36112b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-18 11:22:45 -0700

    Update UserEditQuery.php

--- UserEditQuery.php
+++ UserEditQuery.php
@@ -15,9 +15,17 @@ function UserEditQueryBodyCallback(){
               <i class="material-icons" title="Edit Query">edit</i>
               <i class="material-icons" title="Run Now">flight_takeoff</i>
             </div>
-            <div class="name">Query Name</div>
+            <div class="name">
+              <div class="form-group">
+                <label for="usr">Name:</label>
+                <input type="text" class="form-control" id="usr">
+              </div>
+            </div>
             <div class="lastRun">Last Run</div>
-            <div class="description">Query Description</div>
+            <div class="description">
+              <b>Description:</b><br>
+              <textarea class="AstriaEditor ready" id="description" name="description"></textarea>
+            </div>
             
             <textarea class="AstriaEditor ready" id="query" name="query"></textarea>
             <script>
