13b85da55276f9a3fbad0d6a522183b78ffe972c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-18 10:01:28 -0700

    Create ShowSecuritiesScienceScraperStats.php

---
+++ ShowSecuritiesScienceScraperStats.php
@@ -0,0 +1,9 @@
+<?php
+
+function ShowSecuritiesScienceScraperStats()(){
+  ?>
+  <h2>Scraper Stats</h2>
+  <?php
+  $Stats=Query('SELECT * FORM Statistics','stockhistory');
+  echo ArrTabler($Stats);
+}
