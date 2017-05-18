1c12f1b14907f13de105a46946b8c2e4c93f138a
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-18 13:26:21 -0700

    Update PublicExplore.php

--- PublicExplore.php
+++ PublicExplore.php
@@ -6,7 +6,7 @@ function PublicExploreBodyCallback(){
 <div class="container">
   <div class="row no-gutters">
     <div class="col-md-12">
-      <h4>Explore securities.science</h4>
+      <h4>Check Out These Recent Queries</h4>
       <p>Check us out on <a href="https://github.com/cjtrowbridge/securities.science" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to improve this tool!</p>
     
       <?php
