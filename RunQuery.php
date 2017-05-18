c067aaaddf4d268272b00e410fe6b6284462230e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-18 12:48:50 -0700

    Create RunQuery.php

---
+++ RunQuery.php
@@ -0,0 +1,18 @@
+<?php
+
+function RunQueryBodyCallback(){
+  global $ThisQuery;
+  if(count($ThisQuery)==0{
+    ?>
+  <h1>Query Not Found!</h1>
+    <?php
+  }
+  
+  $Query=$ThisQuery[0];
+  
+  ?>
+  <h1><?php echo $Query['Name']; ?></h1>
+  <p><?php echo nl2br($Query['Name']); ?></p>
+<?php
+
+}
