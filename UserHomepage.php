1a937e912cf7d20984ffc07cb08f45a2b8a75e01
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-18 10:53:54 -0700

    Update UserHomepage.php

--- UserHomepage.php
+++ UserHomepage.php
@@ -9,12 +9,46 @@ function UserHomepageBodyCallback(){
       <h4>Welcome to securities.science</h4>
       <p>Check us out on <a href="https://github.com/cjtrowbridge/securities.science" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to imrpove this tool!</p>
     
-      <p>List my queries and data, offer to create a new query.</p>
+      <h5>My Queries</h5>
       
+      <div class="container">
+        <div class="row no-gutters">
+          <div class="query">
+            <div class="options">
+              <i class="material-icons" title="Delete Query">clear</i>
+              <i class="material-icons" title="Edit Query">edit</i>
+              <i class="material-icons" title="Run Now">flight_takeoff</i>
+            </div>
+            <div class="name">Query Name</div>
+            <div class="lastRun">Last Run</div>
+            <div class="description">Query Description</div>
+          </div>
+        </div>
+      </div>
       
     </div>
   </div>
 </div>
+<style>
+  .query{
+    border: 1px solid #c7c7c7;
+    padding: 1rem;
+    margin-bottom: 1rem;
+  }
+  .query .options{
+    float: right;
+  }
+  .query .name{
+     font-weight: bold;
+  }
+  .query .lastRun{
+     font-style: italic;
+  }
+  .query .description{
+     padding: 1rem;
+     text-indent: 1rem;
+  }
+</style>
 
   <?php
 }
