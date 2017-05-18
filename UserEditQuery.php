150f76798336f46237eca4326f82a932394ca71b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-18 11:27:39 -0700

    Update UserEditQuery.php

--- UserEditQuery.php
+++ UserEditQuery.php
@@ -24,10 +24,10 @@ function UserEditQueryBodyCallback(){
             <div class="lastRun">Last Run</div>
             
             <p><b>Description:</b></p>
-            <textarea class="AstriaEditor ready" id="description" name="description" onmouseup="$(this).css('border-color','red');"></textarea>
+            <textarea class="AstriaEditor ready" id="description" name="description" onmouseup="$('this').css('border-color','red');"></textarea>
             
             <p><b>MySQL Code:</b></p>
-            <textarea class="AstriaEditor ready" id="query" name="query" onmouseup="$(this).css('border-color','red');"></textarea>
+            <textarea class="AstriaEditor ready" id="query" name="query" onmouseup="$('this').css('border-color','red');"></textarea>
             
             <script>
               AstriaEditor();
