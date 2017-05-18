589a36bd40e41ddc58bcf9c3c41cf730a21030c6
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-18 15:29:42 -0700

    Create ExploreTable.php

---
+++ ExploreTable.php
@@ -0,0 +1,39 @@
+<?php
+
+function ExploreTableBodyCallback(){
+  global $ASTRIA;
+
+  if(path(1)==false){
+    echo '<h1>No Table Specified</h1>';
+    return;
+  }
+  
+  $Name = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],path(1));
+  
+  $Description = Query("SELECT * FROM Tables WHERE Name LIKE '".$Name."'",'stockhistory-rea');
+  if(!(isset($Description[0])){
+    echo '<h1>Invalid Table Specified</h1>';
+    return;
+  }
+  $Description = $Description[0];
+  ?>
+  
+  <h1><?php echo $Description['Name']; ?></h1>
+  <p><?php echo $Description['Description']; ?></p>
+  
+  <h2>Table Description</h2>
+  <?php
+  
+  $Describe = Query('describe '.$Description['Name'],'stockhistory-rea');
+  echo ArrTabler($Describe);
+  
+  ?>
+  
+  <h2>Table Contents (Top 100 Rows)</h2>
+  <?php
+  
+  $Describe = Query('SELECT * FROM '.$Description['Name'].' LIMIT 100','stockhistory-rea');
+  echo ArrTabler($Describe);
+  
+  
+}
