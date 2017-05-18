e75f56810f968fa380ccf5d9fc7bb6b4e8a27eeb
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-18 13:53:09 -0700

    Update UserHomepage.php

--- UserHomepage.php
+++ UserHomepage.php
@@ -4,10 +4,9 @@ function UserHomepageBodyCallback(){
   global $ASTRIA; 
 ?>
 
-<div class="container">
   <div class="row no-gutters">
     <div class="col-md-12">
-      <h4>Welcome to securities.science</h4>
+      <h1>Welcome to securities.science</h1>
       <p>Check us out on <a href="https://github.com/cjtrowbridge/securities.science" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to imrpove this tool!</p>
     
       <h1>My Queries</h1>
@@ -42,7 +41,6 @@ function UserHomepageBodyCallback(){
       
     </div>
   </div>
-</div>
 
   <?php
 }
