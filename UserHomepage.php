3691d5d1d1102c2ac4260486308f36e212b5c34b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-18 11:46:53 -0700

    Update UserHomepage.php

--- UserHomepage.php
+++ UserHomepage.php
@@ -9,9 +9,9 @@ function UserHomepageBodyCallback(){
       <h4>Welcome to securities.science</h4>
       <p>Check us out on <a href="https://github.com/cjtrowbridge/securities.science" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to imrpove this tool!</p>
     
-      <h1>My Queries <a href="/edit-query"><i class="material-icons">add</i></a></h1>
+      <h1>My Queries <a href="/create-query"><i class="material-icons">add</i></a></h1>
       <p>This page shows all the queries you are working on and lets you run or edit them.</p>
-      <p>You can <a href="/edit-query">create a new query now</a> or check out some queries <a href="/explore">other people are working on</a>.</p>
+      <p>You can <a href="/create-query">create a new query now</a> or check out some queries <a href="/explore">other people are working on</a>.</p>
       
       <div class="row no-gutters">
         <div class="query">
