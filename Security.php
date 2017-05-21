b11672f8f930a1a74bef881a5d1286bc016f4987
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-21 10:39:19 -0700

    Create Security.php

---
+++ Security.php
@@ -0,0 +1,16 @@
+<?php
+
+function SecurityBodyCallback(){
+  $Title = 'Securities';
+  if(!(path(1)==false)){
+    $Symbol = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],path(1));
+    $Symbol=strtoupper($Symbol);
+    $Symbol=Query("SELECT Symbol FROM Security WHERE upper(Symbol) LIKE '".$Symbol."'");
+    if(isset($Symbol[0])){
+      $Title=$Symbol;
+    }
+  }
+ ?>
+ <h1><?php echo $Title; ?></h1>
+ <?php
+}
