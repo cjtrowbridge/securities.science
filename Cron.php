abf672c7df950b7a92a317baa1873e4336d22f79
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-23 20:41:12 -0700

    Update Cron.php

--- Cron.php
+++ Cron.php
@@ -2,15 +2,15 @@
 
 Hook('Hourly Cron','SecuritiesScienceHourlyCron();');
 function SecuritiesScienceHourlyCron(){
-  SendEmail('This is an hourly cron alert.','chris.j.trowbridge@gmail.com');
+  SendEmail('This is an hourly cron alert.','cron','chris.j.trowbridge@gmail.com');
 }
 
 Hook('Daily Cron','SecuritiesScienceDailyCron();');
 function SecuritiesScienceDailyCron(){
-  SendEmail('This is a daily cron alert.','chris.j.trowbridge@gmail.com');
+  SendEmail('This is a daily cron alert.','cron','chris.j.trowbridge@gmail.com');
 }
 
 Hook('Weekly Cron','SecuritiesScienceWeeklyCron();');
 function SecuritiesScienceWeeklyCron(){
-  SendEmail('This is a weekly cron alert.','chris.j.trowbridge@gmail.com');
+  SendEmail('This is a weekly cron alert.','cron','chris.j.trowbridge@gmail.com');
 }
