ca604cf50389f439d4d49007eac4df28757f3bdf
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 15:29:47 -0700

    Update ParseFetches.php

--- ParseFetches.php
+++ ParseFetches.php
@@ -5,7 +5,10 @@ function SSParseFetches(){
 }
 
 function SSParseFetchesNow(){
-  $Data = Query("SELECT * FROM FeedFetch ORDER BY FetchID DESC LIMIT 1");
-  echo strlen($Data[0]['Content']);
-  echo $Data[0]['Content'];
+  //$Data = Query("SELECT * FROM FeedFetch ORDER BY FetchID DESC LIMIT 1");
+  //echo strlen($Data[0]['Content']);
+  //echo $Data[0]['Content'];
+  $Data = FetchURL('https://api.robinhood.com/quotes/historicals/DUST/?interval=5minute&span=week&bounds=regular');
+  echo strlen($Data);
+  echo $Data;
 }
