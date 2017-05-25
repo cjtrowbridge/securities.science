c54f9f84991c2a4d88d65d5dcb8f533100870aca
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 15:26:01 -0700

    Update ParseFetches.php

--- ParseFetches.php
+++ ParseFetches.php
@@ -6,5 +6,5 @@ function SSParseFetches(){
 
 function SSParseFetchesNow(){
   $Data = Query("SELECT * FROM FeedFetch ORDER BY FetchID DESC LIMIT 1");
-  echo $Data['Content'];
+  echo $Data[0]['Content'];
 }
