e2d7e9dba21105aef1ff4ad2ac22e75969c5cbcd
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 15:30:51 -0700

    Update ParseFetches.php

--- ParseFetches.php
+++ ParseFetches.php
@@ -9,6 +9,7 @@ function SSParseFetchesNow(){
   //echo strlen($Data[0]['Content']);
   //echo $Data[0]['Content'];
   $Data = FetchURL('https://api.robinhood.com/quotes/historicals/DUST/?interval=5minute&span=week&bounds=regular');
+  $Data  = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$Data);
   echo strlen($Data);
   echo $Data;
 }
