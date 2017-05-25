f78bd62ba34e1b96a2fcdb1c15b0fcfac6b526ec
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 15:34:20 -0700

    Update ParseFetches.php

--- ParseFetches.php
+++ ParseFetches.php
@@ -8,9 +8,31 @@ function SSParseFetchesNow(){
   //$Data = Query("SELECT * FROM FeedFetch ORDER BY FetchID DESC LIMIT 1");
   //echo strlen($Data[0]['Content']);
   //echo $Data[0]['Content'];
-  $Data = FetchURL('https://api.robinhood.com/quotes/historicals/DUST/?interval=5minute&span=week&bounds=regular');
+  
   global $ASTRIA;
-  $Data  = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$Data);
-  echo strlen($Data);
-  echo $Data;
+  
+  $Start    = microtime(true);
+  $Content  = FetchURL('https://api.robinhood.com/quotes/historicals/DUST/?interval=5minute&span=week&bounds=regular');
+  $End      = microtime(true);
+  
+  $Duration = $End - $Start;
+  $URL      = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],'https://api.robinhood.com/quotes/historicals/DUST/?interval=5minute&span=week&bounds=regular');
+  $Content  = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$Content);
+  $Expires=date('Y-m-d H:i:s',(time()+$Feed['TTL']));
+  
+  $SQL="
+    INSERT INTO `FeedFetch` (
+      `FeedID`, `URL`, `Arguments`, `FetchTime`, `Duration`, `Content`, `ContentLength`, `Expires`
+    ) VALUES (
+      '".$Feed['FeedID']."', 
+      '".$URL."', 
+      NULL /* TODO: Arguments (This is complicated and not immediately necessary.) */, 
+      NOW(), 
+      '".$Duration."', 
+      '".$Content."',
+      '".strlen($Content)."',
+      '".$Expires."'
+    );
+  ";
+  pd(Query($SQL));
 }
