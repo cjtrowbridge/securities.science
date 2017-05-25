32738beebdb0a9e9a9c9140ebbc4b555a9562936
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 15:58:19 -0700

    Update ParseFetches.php

--- ParseFetches.php
+++ ParseFetches.php
@@ -6,4 +6,7 @@ function SSParseFetches(){
 
 function SSParseFetchesRobinhood(){
   $Data = Query("SELECT * FROM FeedFetch WHERE URL LIKE '%robinhood%' ORDER BY FetchID DESC LIMIT 1");
+  foreach($Data as $Fetch){
+    pd($Fetch);
+  }
 }
