201c3cb0b09b7155022b37c837e5ade20dfc8d04
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-26 08:07:43 -0700

    Update ParseFetches.php

--- ParseFetches.php
+++ ParseFetches.php
@@ -10,12 +10,31 @@ function SSParseFetchesRobinhood(){
     $MostRecent = Query("SELECT MAX(TradingDate) as 'LastDate' FROM Daily WHERE Symbol LIKE '".$Security['Symbol']."'","stockhistory");
     $LastDate = strtotime($MostRecent[0]['LastDate']);
     
-    $Data = Query("SELECT * FROM FeedFetch WHERE URL LIKE '%robinhood%' AND FetchTime > '".$LastDate."' ORDER BY FetchID DESC LIMIT 1");
+    while($LastDate < time()){
+      //check for data
+      $Data = Query("SELECT * FROM FeedFetch WHERE URL LIKE '%robinhood%' AND FetchTime LIKE '%".date('Y-m-d',$LastDate)."T16%' ORDER BY FetchID DESC LIMIT 1");
+      pd($Date);
+      //add to dailies
+      
+      //plus one day
+      $LastDate += (60*60*24);
+    }
+    
+    /*
+    $Data = Query("SELECT * FROM FeedFetch WHERE URL LIKE '%robinhood%' AND FetchTime > '".date('Y-m-d',$LastDate)."' ORDER BY FetchID DESC LIMIT 1");
     foreach($Data as $Fetch){
       $Content = $Fetch['Content'];
       $JSON = json_decode($Content,true);
-      pd($JSON);
+      foreach($JSON['Historical'] as $Quote){
+        $QuoteTime = strtotime($Quote['begins_at']);
+        if(
+          ($QuoteTime > $LastDate) &&
+          (date('H',$QuoteTime) > 16
+        ){
+          
+        }
+      }
     }
-    
+  */
   }
 }
