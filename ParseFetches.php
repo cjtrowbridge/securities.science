f62b7747e79b0e208b70f81754d3e3b255dfa303
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-26 07:58:23 -0700

    Update ParseFetches.php

--- ParseFetches.php
+++ ParseFetches.php
@@ -5,8 +5,17 @@ function SSParseFetches(){
 }
 
 function SSParseFetchesRobinhood(){
-  $Data = Query("SELECT * FROM FeedFetch WHERE URL LIKE '%robinhood%' ORDER BY FetchID DESC LIMIT 1");
-  foreach($Data as $Fetch){
-    pd($Fetch);
+  $Securities = Query("SELECT * FROM Security");
+  foreach($Securities as $Security){
+    $MostRecent = Query("SELECT MAX(TradingDate) as 'LastDate' FROM Daily WHERE Symbol LIKE '".$Security['Symbol']."'","stockhistory");
+    $LastDate = strtotime($MostRecent[0]['LastDate']);
+    
+    $Data = Query("SELECT * FROM FeedFetch WHERE URL LIKE '%robinhood%' AND FetchTime > '".$LastDate."' ORDER BY FetchID DESC LIMIT 1");
+    foreach($Data as $Fetch){
+      $Content = $Fetch['Content'];
+      $JSON = json_decode($Content,true);
+      pd($JSON);
+    }
+    
   }
 }
