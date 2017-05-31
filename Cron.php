4e19616a6966387174144c4d18b88c8f7b89eaaa
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-31 00:54:36 -0700

    Update Cron.php

--- Cron.php
+++ Cron.php
@@ -7,10 +7,10 @@ function SecuritiesScienceHourlyCron(){
 
 Hook('Daily Cron','SecuritiesScienceDailyCron();');
 function SecuritiesScienceDailyCron(){
-  SendEmail('This is a daily cron alert.','cron','chris.j.trowbridge@gmail.com');
+  //SendEmail('This is a daily cron alert.','cron','chris.j.trowbridge@gmail.com');
 }
 
 Hook('Weekly Cron','SecuritiesScienceWeeklyCron();');
 function SecuritiesScienceWeeklyCron(){
-  SendEmail('This is a weekly cron alert.','cron','chris.j.trowbridge@gmail.com');
+  //SendEmail('This is a weekly cron alert.','cron','chris.j.trowbridge@gmail.com');
 }
