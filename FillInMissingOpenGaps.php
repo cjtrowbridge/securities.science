f416092c6080102f2389aa17ac5ffdef6a16e74f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-16 13:42:18 -0700

    Update FillInMissingOpenGaps.php

--- FillInMissingOpenGaps.php
+++ FillInMissingOpenGaps.php
@@ -3,23 +3,23 @@
 function FillInMissingOpenGaps(){
   
   //Find a chunk of them that have not been filled in
-  $Missings = Query("SELECT * FROM DailyQuotesWithRSI WHERE OpenGap IS NULL ORDER BY TradingDate DESC");
+  $Missings = Query("SELECT * FROM DailyQuotesWithRSI WHERE OpenGap IS NULL ORDER BY TradingDate DESC",'stockhistory');
   
   foreach($Missings as $Missing){
     
     //Get the previous days' data
-    $Data = Query("SELECT * FROM DailyQuotesWithRSI WHERE Symbol LIKE '".$Missing['Symbol']."' AND TradingDate < '".$Missing['TradingDate']."' ORDER BY TradingDate DESC LIMIT 1");
+    $Data = Query("SELECT * FROM DailyQuotesWithRSI WHERE Symbol LIKE '".$Missing['Symbol']."' AND TradingDate < '".$Missing['TradingDate']."' ORDER BY TradingDate DESC LIMIT 1",'stockhistory');
     
     //If there is not at least one previous day of data, skip this one.
     if(count($Data)<1){
-      Query("UPDATE DailyQuotesWithRSI SET OpenGap = 0 WHERE DailyQuoteWithRSIID = ".$Missing['DailyQuoteWithRSIID']);
+      Query("UPDATE DailyQuotesWithRSI SET OpenGap = 0 WHERE DailyQuoteWithRSIID = ".$Missing['DailyQuoteWithRSIID'],'stockhistory');
       continue;
     }
     
     $Gap = $Missing['Open'] - $Data[0]['Close'];
     
     
-    $SQL="UPDATE DailyQuotesWithRSI SET OpenGap = '".$Gap."' WHERE DailyQuoteWithRSIID = ".$Missing['DailyQuoteWithRSIID'];
+    $SQL="UPDATE DailyQuotesWithRSI SET OpenGap = '".$Gap."' WHERE DailyQuoteWithRSIID = ".$Missing['DailyQuoteWithRSIID'],'stockhistory';
     Query($SQL);
     
   }
