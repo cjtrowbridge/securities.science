3f25822fc2bda816d7b70c42f88f6b6912fbf5e3
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-18 06:51:40 -0700

    Update Cron.php

--- Cron.php
+++ Cron.php
@@ -3,14 +3,14 @@
 Hook('Hourly Cron','SecuritiesScienceHourlyCron();');
 function SecuritiesScienceHourlyCron(){
   //SendEmail('This is an hourly cron alert.','cron','chris.j.trowbridge@gmail.com');
-  if(file_exists('plugins/securities.science/InsertWorker.php')){
-    include('plugins/securities.science/InsertWorker.php');
-  }
 }
 
 Hook('Daily Cron','SecuritiesScienceDailyCron();');
 function SecuritiesScienceDailyCron(){
   //SendEmail('This is a daily cron alert.','cron','chris.j.trowbridge@gmail.com');
+  if(file_exists('plugins/securities.science/InsertWorker.php')){
+    include('plugins/securities.science/InsertWorker.php');
+  }
 }
 
 Hook('Weekly Cron','SecuritiesScienceWeeklyCron();');
