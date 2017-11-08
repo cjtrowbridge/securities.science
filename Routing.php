bd17c64fb994096d5f794d3555f7d576562b816e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-07 20:04:20 -0800

    Update Routing.php

--- Routing.php
+++ Routing.php
@@ -68,7 +68,7 @@ Hook('User Is Logged In - Before Presentation','UserPageBefore();');
 
 function UserPageBefore(){
   include_once('ShowSecuritiesScienceScraperStats.php');
-  Hook('Architect Homepage','ShowSecuritiesScienceScraperStats();');
+  //Hook('Architect Homepage','ShowSecuritiesScienceScraperStats();');
   Nav('main-logged-in','link','Explore','/explore');
 }
 
